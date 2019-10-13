3e3db52bcab9f5df31191ba951f63e7bfaebb438
jbobinac <56370125+jbobinac@users.noreply.github.com>
2019-10-13 12:55:21 +0200

    Update Exercise_1_2.m

--- Exercise_1_2.m
+++ Exercise_1_2.m
@@ -7,17 +7,19 @@
 % Inputs
 x =[0,1,4];
 f =[0,2,8];
-% evaluating knot
-e_k = 2;
+% evaluating point
+e_p = 2;
 
-R = neville_scheme(x,f,2);
+R = neville_scheme(x,f,e_p);
 
 
 
 
 function R = neville_scheme (knots,d_v,e_k)
 
+% polynomial degree
 n = length(knots) - 1;
+
 % Output matrix
 R = zeros(n+1, n+1);
 
@@ -28,8 +30,9 @@ end
 
 % Fill in the rest of the columns by the Neville scheme definition
 for j = 2:n+1
-    for i = 1:n-j+2
-        R(i,j) = ((e_k - knots(i))* R(i+1,j-1) - (e_k - knots(i+j-1))* R(i,j-1)) / (knots(i+j-1)-knots(i));
+    m = j - 1; % polynomial degree of the column
+    for i = 1:n-m+1
+        R(i,j) = ((e_k - knots(i))* R(i+1,m) - (e_k - knots(i+m))* R(i,m)) / (knots(i+m)-knots(i));
     end
 end
 end
